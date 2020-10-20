10a3a5cfcf2935b25c7d55edd021587e2ddb4f02
CNMan <nklijl@mail.nankai.edu.cn>
2020-10-20 16:57:35 +0800

    Tue 20 Oct 2020 04:57:35 PM HKT

--- update.sh
+++ update.sh
@@ -9,12 +9,22 @@ curl https://www.amazon.com/update_Kindle_Paperwhite_6th_Gen | sed 's/"/\n/g' | 
 curl https://www.amazon.com/update_Kindle_Paperwhite_7th_Gen | sed 's/"/\n/g' | grep bin >>tmp
 curl https://www.amazon.com/update_Kindle_Paperwhite_10th_Gen | sed 's/"/\n/g' | grep bin >>tmp
 curl https://www.amazon.com/update_Kindle_Voyage_7th_Gen | sed 's/"/\n/g' | grep bin >>tmp
-wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_oasis/AAHKV2X7AFYLW/zh_CN.html -O oasisv1_zh.html
-wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_all_new_oasis/AAHKV2X7AFYLW/zh_CN.html -O oasisv2_zh.html
-wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_all_new_oasis_v2/AAHKV2X7AFYLW/zh_CN.html -O oasisv3_zh.html
-wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_oasis/ATVPDKIKX0DER/en_US.html -O oasisv1_en.html
-wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_all_new_oasis/ATVPDKIKX0DER/en_US.html -O oasisv2_en.html
-wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_all_new_oasis_v2/ATVPDKIKX0DER/en_US.html -O oasisv3_en.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_all_new_oasis_v2/AAHKV2X7AFYLW/zh_CN.html -O Kindle_Oasis_10th_zh.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_all_new_oasis_v2/ATVPDKIKX0DER/en_US.html -O Kindle_Oasis_10th_en.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_all_new_oasis/AAHKV2X7AFYLW/zh_CN.html -O Kindle_Oasis_9th_zh.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_all_new_oasis/ATVPDKIKX0DER/en_US.html -O Kindle_Oasis_9th_en.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_oasis/AAHKV2X7AFYLW/zh_CN.html -O Kindle_Oasis_8th_zh.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_oasis/ATVPDKIKX0DER/en_US.html -O Kindle_Oasis_8th_en.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_voyage/AAHKV2X7AFYLW/zh_CN.html -O Kindle_Voyage_7th_zh.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_voyage/ATVPDKIKX0DER/en_US.html -O Kindle_Voyage_7th_en.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_all_new_paperwhite_v2/AAHKV2X7AFYLW/zh_CN.html -O Kindle_Paperwhite_10th_zh.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_all_new_paperwhite_v2/ATVPDKIKX0DER/en_US.html -O Kindle_Paperwhite_10th_en.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_all_new_paperwhite/AAHKV2X7AFYLW/zh_CN.html -O Kindle_Paperwhite_7th_zh.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_all_new_paperwhite/ATVPDKIKX0DER/en_US.html -O Kindle_Paperwhite_7th_en.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_10th/AAHKV2X7AFYLW/zh_CN.html -O Kindle_10th_zh.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_10th/ATVPDKIKX0DER/en_US.html -O Kindle_10th_en.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_8th/AAHKV2X7AFYLW/zh_CN.html -O Kindle_8th_zh.html
+wget https://s3.amazonaws.com/firmwarereleasenotes/update_kindle_8th/ATVPDKIKX0DER/en_US.html -O Kindle_8th_en.html
 cat tmp | sort | uniq >KindleE-ReaderSoftwareUpdates
 rm tmp
 git add .
